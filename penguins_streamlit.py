24d3a1b422ba0af5a6ae62e1b3b13a6d9998d621
czokw1 <wojciech.czok@gmail.com>
2022-04-24 22:56:11 +0200

    code review

--- penguins_streamlit.py
+++ penguins_streamlit.py
@@ -17,76 +17,64 @@ st.write("This app uses 6 inputs to predict the species of penguin using "
 
   
 
-password_guess = st.text_input('What is the Password?') 
+# password_guess = st.text_input('What is the Password?') 
 
-if password_guess != 'streamlit': 
-  st.stop() 
+# if password_guess != 'streamlit': 
+#   st.stop() 
 
   
 
 penguin_file = st.file_uploader('Upload your own penguin data') 
 
-if penguin_file is None: 
+if penguin_file is None:
+  rf_pickle = open('random_forest_penguin.pickle', 'rb') 
 
-    rf_pickle = open('random_forest_penguin.pickle', 'rb') 
+  map_pickle = open('output_penguin.pickle', 'rb') 
 
-    map_pickle = open('output_penguin.pickle', 'rb') 
+  rfc = pickle.load(rf_pickle) 
 
-    rfc = pickle.load(rf_pickle) 
+  unique_penguin_mapping = pickle.load(map_pickle) 
 
-    unique_penguin_mapping = pickle.load(map_pickle) 
+  rf_pickle.close() 
 
-    rf_pickle.close() 
+  map_pickle.close() 
 
-    map_pickle.close() 
+else:
+  penguin_df = pd.read_csv(penguin_file) 
 
-else: 
+  penguin_df = penguin_df.dropna() 
 
-    penguin_df = pd.read_csv(penguin_file) 
+  output = penguin_df['species'].values 
 
-    penguin_df = penguin_df.dropna() 
+  features = penguin_df[['island', 'bill_length_mm', 'bill_depth_mm','flipper_length_mm', 'body_mass_g', 'sex']] 
 
-    output = penguin_df['species'] 
+  features = pd.get_dummies(features).values 
 
-    features = penguin_df[['island', 'bill_length_mm', 'bill_depth_mm', 
-
-                           'flipper_length_mm', 'body_mass_g', 'sex']] 
-
-    features = pd.get_dummies(features) 
-
-    output, unique_penguin_mapping = pd.factorize(output) 
+  output, unique_penguin_mapping = pd.factorize(output) 
 
  
 
-    x_train, x_test, y_train, y_test = train_test_split( 
+  x_train, x_test, y_train, y_test = train_test_split(features, output, test_size=.8) 
 
-        features, output, test_size=.8) 
+  rfc = RandomForestClassifier(random_state=15) 
 
-    rfc = RandomForestClassifier(random_state=15) 
+  rfc.fit(x_train, y_train) 
 
-    rfc.fit(x_train, y_train) 
+  y_pred = rfc.predict(x_test) 
 
-    y_pred = rfc.predict(x_test) 
+  score = round(accuracy_score(y_pred, y_test), 2) 
 
-    score = round(accuracy_score(y_pred, y_test), 2) 
-
-    st.write('We trained a Random Forest model on these data,' 
+  st.write('We trained a Random Forest model on these data,' 
              ' it has a score of {}! Use the ' 
              'inputs below to try out the model.'.format(score))
 
 with st.form('user_inputs'): 
-  island = st.selectbox('Penguin Island', options=[
-    'Biscoe', 'Dream', 'Torgerson']) 
-  sex = st.selectbox('Sex', options=[
-    'Female', 'Male']) 
-  bill_length = st.number_input(
-    'Bill Length (mm)', min_value=0) 
-  bill_depth = st.number_input(
-    'Bill Depth (mm)', min_value=0) 
-  flipper_length = st.number_input(
-    'Flipper Length (mm)', min_value=0) 
-  body_mass = st.number_input(
-    'Body Mass (g)', min_value=0) 
+  island = st.selectbox('Penguin Island', options=['Biscoe', 'Dream', 'Torgerson']) 
+  sex = st.selectbox('Sex', options=['Female', 'Male']) 
+  bill_length = st.number_input('Bill Length (mm)', min_value=0) 
+  bill_depth = st.number_input('Bill Depth (mm)', min_value=0) 
+  flipper_length = st.number_input('Flipper Length (mm)', min_value=0) 
+  body_mass = st.number_input('Body Mass (g)', min_value=0) 
   st.form_submit_button() 
 
 
