107baca0397a8e8c498b3460617123d3e249ce52
czokw1 <wojciech.czok@gmail.com>
2022-04-24 23:25:40 +0200

    adding password

--- penguins_streamlit.py
+++ penguins_streamlit.py
@@ -17,11 +17,11 @@ st.write("This app uses 6 inputs to predict the species of penguin using "
 
   
 
-# password_guess = st.text_input('What is the Password?') 
+password_guess = st.text_input('What is the Password?') 
 
-# if password_guess != 'streamlit': 
-#   st.stop() 
-
+if password_guess != st.secrets["password"]: 
+  st.stop() 
+  print('Please provide correct password')
   
 
 penguin_file = st.file_uploader('Upload your own penguin data') 
